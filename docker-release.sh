3dbed8248dfeaa364ad86f70635feeac7b4a7ad2
tdedobbeleer <tdedobbeleer@gmail.com>
2024-06-17 19:52:35 +0200

    Update docker-release.sh

--- docker-release.sh
+++ docker-release.sh
@@ -2,7 +2,7 @@
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
 build () {
-  if ! docker buildx build --push --platform linux/arm64/v8,linux/amd64 --tag "${DOCKER_USER}/nicehash:${1}" .; then
+  if ! docker buildx build --push --platform linux/arm64/v8,linux/amd64 --tag "${DOCKER_USER}/fluvius:${1}" .; then
     echo "Building tag ${1} failed miserably."
     exit 1
   fi
